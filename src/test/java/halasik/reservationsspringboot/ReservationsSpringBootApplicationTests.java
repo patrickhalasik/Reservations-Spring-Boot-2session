f713bad049c56fa9bfd41e767dc00e394b82b0ce
Patrick Halasik <patrickhalasik@gmail.com>
2021-08-09 22:24:42 +0100

    Changement d'IDE: IntelliJ

---
+++ src/test/java/halasik/reservationsspringboot/ReservationsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package halasik.reservationsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
